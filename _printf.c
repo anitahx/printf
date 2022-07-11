019bd55bc8d9b03d5167b5000e2dd63a814e16db
anitahx <anitaizzy4@gmail.com>
2022-07-11 01:15:42 -0700

    a function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,71 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+  * find_function - function that finds formats for _printf
+  * @format: format (char, string, int, dec)
+  * Return: 0
+  */
+
+int (*find_function(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	while (find_f[i].sc)
+	{
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+  * _printf - function to produce output
+  * @format: format (char, string, int, dec)
+  * Return: size of output text
+  */
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			cprint++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(list);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(list);
+	return (cprint);
+}
---
+++ main.h
@@ -0,0 +1,51 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+
+
+/**
+  * struct code_format - Struct format
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
+#endif
+/**
+  * struct code_format - Struct format
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
+#endif
